c44a503438672f97a8202d998da82d93ebc78c58
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-03-05 18:13:00 -0300

    feat: added home page

---
+++ src/components/home/index.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+import { MainComponent } from "../MainComponent"
+
+export const Home = () => {
+
+    return (
+
+        <Main>
+            <h1>Welcome to McDonalds' Hub</h1>
+        </Main>
+    )
+}
+
+const Main = styled.main`
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: var(--red);
+    color: var(--yellow);
+    transition: 0.15s ease-in;
+`
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/login";
 import { Register } from "../components/register";
+import { Home } from "../components/home";
 
 const AppRouter = () => {
     
     return (
         <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} /> 
         </Routes>
